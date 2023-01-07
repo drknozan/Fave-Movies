41ad15c49d0e9339749d330252f5d76aa89411a5
drknozan <drknozan@gmail.com>
2023-01-07 19:36:39 +0300

    feat: footer

---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return(
+        <div className="text-gray-600 text-center bg-slate-900 py-6 h-20 w-full">
+            Made by <a href="https://github.com/drknozan" className="text-gray-400 ">drknozan</a> in 2022.
+        </div>
+    )
+}
+
+export default Footer;
