72beb77ecd0e83fc37ae4e36a30cbb11fd0b8772
drknozan <drknozan@gmail.com>
2023-02-10 20:22:10 +0300

    feat: sort search results by rate

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -7,7 +7,7 @@ import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 function Search() {
     const [searchParams, setSearchParams] = useSearchParams();
     const [result, setResult] = useState(null);
-
+    const [rateSort, setRateSort] = useState(false);
     const genre = searchParams.get("genre") || "";
     const title = searchParams.get("title") || "";
 
@@ -18,6 +18,10 @@ function Search() {
             if (genre) {
                 let reqUrl = `http://localhost:5000/api/movies/?genre=${genre}`;
                 
+                if (rateSort) {
+                    reqUrl += '&sortByRate=descending';
+                }
+
                 try {
                     const { data } = await axios.get(reqUrl);
                     setResult(data.movies);
@@ -29,6 +33,10 @@ function Search() {
             if (title) {
                 let reqUrl = `http://localhost:5000/api/movies/?title=${title}`;
 
+                if (rateSort) {
+                    reqUrl += `&sortByRate=descending`;
+                }
+
                 try {
                     const { data } = await axios.get(reqUrl);
                     setResult(data.movies);
@@ -39,14 +47,14 @@ function Search() {
         }
 
         getMovies();
-    }, [searchParams]);
+    }, [searchParams, rateSort]);
 
     return (
         <div className="w-screen bg-slate-800">
             <div className="flex">
                 <div className="w-4/5 mx-auto">
                     <label className="inline-flex relative items-center cursor-pointer mt-4">
-                        <input type="checkbox" value="" className="sr-only peer" />
+                        <input type="checkbox" value="" className="sr-only peer" onChange={e => setRateSort(e.target.checked)}/>
                         <div className="w-11 h-6 bg-slate-600 peer-focus:outline-none rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-slate-900"></div>
                         <span className="ml-3 text-sm font-medium text-gray-900 text-gray-300">Sort By Rate</span>
                     </label>
