19bd44e58efda53dbd721d4643d709e4062fb8c8
drknozan <drknozan@gmail.com>
2023-02-13 19:45:43 +0300

    feat: list movies by genre on home page

---
+++ client/src/components/MovieCarousel.js
@@ -0,0 +1,59 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+function MovieCarousel({ genre }) {
+    const [movies, setMovies] = useState(null);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const getMoviesByGenre = async () => {
+            const { data } = await axios.get(`http://localhost:5000/api/movies/?genre=${genre}`);
+            setMovies(data.movies);
+        }
+
+        getMoviesByGenre();
+    }, []);
+
+    return (
+        <div className="bg-slate-900 px-6 py-8 rounded">
+            <div className="flex justify-between">
+                <span className="text-white font-bold text-xl block mb-4">{genre}</span>
+                <span onClick={() => navigate(`/search?genre=${genre}`)} className="text-white cursor-pointer border rounded bg-slate-900 flex items-center mb-2 text-xs p-2 hover:bg-slate-700">See all results</span>
+            </div>
+            <Swiper
+                slidesPerView={1}
+                spaceBetween={30}
+                breakpoints={{
+                    640: {
+                        slidesPerView: 3,
+                    },
+                    1024: {
+                        slidesPerView: 5,
+                    }
+                }}
+                navigation={true}
+                modules={[ Navigation ]}
+                className="mySwiper h-80"
+                >
+                    {
+                        movies &&
+                        movies.map((e) => {
+                            return (
+                                <SwiperSlide className="bg-slate-400 cursor-pointer rounded" key={e.no} onClick={() => { navigate(`/movie/${e.no}`); }}>
+                                    <img className="w-full h-full object-cover" src={e.poster_url} />
+                                </SwiperSlide>
+                            )
+                        })
+                    }
+            </Swiper>
+        </div>
+    )
+}
+
+export default MovieCarousel;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,30 @@
+import MovieCarousel from "../components/MovieCarousel";
 
 function Home() {
+    const categories = [
+        "Sci-Fi",
+        "Adventure",
+        "Comedy",
+        "Drama",
+        "Action",
+        "Crime",
+        "Romance",
+        "Mystery",
+        "History",
+        "Biography",
+        "Fantasy",
+        "War",
+        "Animation",
+        "Thriller",
+    ];
 
     return(
-        <div>
-
+        <div className="flex flex-col w-4/5 mx-auto my-6 gap-12">
+            {
+                categories.map((e) => {
+                    return <MovieCarousel genre={e} key={e}></MovieCarousel>
+                })
+            }
         </div>
     )
 }
